a7429fb98507927fb31c3bd9cc67a066eecc95f0
Dimitris Carvalho Calixto <68653065+dimitriscalixto@users.noreply.github.com>
2024-07-15 18:49:55 -0500

    cd front-end

--- sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -14,7 +14,9 @@ public class FiltroCors {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:4200", "http://127.0.0.1:8080"));
+        config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:4200", "http://127.0.0.1:8080", "https://integracao-continua-t5-1oq9.onrender.com\r\n" + //
+                        "\r\n" + //
+                        ""));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
